4e2c3c9ad413eacd98ad0ebd777d5aa11070d3e0
unknown <viniciusbeloti7@gmail.com>
2020-10-13 08:24:00 -0300

    chore: enable decoratos

--- src/database/migrations/1602587586320-create_orphanages.ts
+++ src/database/migrations/1602587586320-create_orphanages.ts
@@ -39,6 +39,10 @@ export default class createOrphanages1602587586320 implements MigrationInterface
           type: 'text'
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: 'false'
