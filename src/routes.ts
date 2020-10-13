cd175ff3dbdb72de0fb03d884670d88240d0de13
unknown <viniciusbeloti7@gmail.com>
2020-10-13 09:24:24 -0300

    chore: create relationship between Orphanage and Images table

--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,10 @@ import OrphanagesController from './controllers/OrphanagesController';
 import uploadConfig from './config/upload';
 
 const routes = Router();
-const uplaod = multer(uploadConfig);
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 export default routes;
