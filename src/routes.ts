4c98c1ef2d3606a3890d6b05a513a1023d95d0f1
unknown <viniciusbeloti7@gmail.com>
2020-10-13 08:57:37 -0300

    chore: add multer config

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
-
 import { Router } from 'express';
+import multer from 'multer';
 import OrphanagesController from './controllers/OrphanagesController';
+import uploadConfig from './config/upload';
 
 const routes = Router();
+const uplaod = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
