a50f726d72b0ae785139a498ba562d4c19734f80
unknown <viniciusbeloti7@gmail.com>
2020-10-13 08:35:35 -0300

    chore: create orphanage on /orphanages

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,39 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
 
 import './database/connection';
+import Orphanage from './models/Orphanage';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  return res.json({ ok: true})
+app.post('/orphanages', async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = req.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return res.json({ orphanage })
 })
 
 app.listen(3333);
